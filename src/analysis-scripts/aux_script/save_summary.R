2fbe97af63af63e9e3a90202e34bd0adc57787bc
sroyos <sroyos@iacs.es>
2024-02-27 17:21:20 +0100

    Update version
    Added analysis by state for USA.
    Running: 'analysis_icconic_hipfx_timetosurgery_by_residence_area_USA'

--- src/analysis-scripts/aux_script/save_summary.R
+++ src/analysis-scripts/aux_script/save_summary.R
@@ -1,4 +1,4 @@
-sink("../../outputs/summary_GAMM_exitus_inhospital.txt")
+sink(paste0("../../outputs/summary_GAMM_exitus_inhospital",params$state,".txt"))
 print(summary(model_exitus_hosp_bl))
 print(paste0('For component hospital_st, variance is: ',round(var_re_exitus_inhospital$variance,5), 
              ' with lower_ci95=',round(var_re_exitus_inhospital$lower_ci,5),' and upper_ci95=',round(var_re_exitus_inhospital$upper_ci,5)))
@@ -20,7 +20,7 @@ print(paste0('Summary for country_cd: ',descriptive_values$country_cd))
 sink(file = NULL)
 
 
-sink("../../outputs/summary_GAMM_exitus_7days.txt")
+sink(paste0("../../outputs/summary_GAMM_exitus_7days",params$state,".txt"))
 print(summary(model_exitus_7days_bl))
 print(paste0('For component hospital_st, variance is: ',round(var_re_exitus_7d$variance,5), 
              ' with lower_ci95=',round(var_re_exitus_7d$lower_ci,5),' and upper_ci95=',round(var_re_exitus_7d$upper_ci,5)))
@@ -40,7 +40,7 @@ print(paste0('MOR with bootsrapping: ',round(MOR_w_CI_exitus_7d[2],5),
 print(paste0('Summary for country_cd: ',descriptive_values$country_cd))
 sink(file = NULL)
 
-sink("../../outputs/summary_GAMM_exitus_30days.txt")
+sink(paste0("../../outputs/summary_GAMM_exitus_30days",params$state,".txt"))
 print(summary(model_exitus_30days_bl))
 print(paste0('For component hospital_st, variance is: ',round(var_re_exitus_30d$variance,5), 
              ' with lower_ci95=',round(var_re_exitus_30d$lower_ci,5),' and upper_ci95=',round(var_re_exitus_30d$upper_ci,5)))
@@ -62,7 +62,7 @@ print(paste0('Summary for country_cd: ',descriptive_values$country_cd))
 sink(file = NULL)
 
 
-sink("../../outputs/summary_GAMM_unplanned_7days.txt")
+sink(paste0("../../outputs/summary_GAMM_unplanned_7days",params$state,".txt"))
 print(summary(model_unplanned_7days_bl))
 print(paste0('For component hospital_st, variance is: ',round(var_re_unplanned_7d$variance,5), 
              ' with lower_ci95=',round(var_re_unplanned_7d$lower_ci,5),' and upper_ci95=',round(var_re_unplanned_7d$upper_ci,5)))
@@ -86,7 +86,7 @@ sink(file = NULL)
 
 
 
-sink("../../outputs/summary_GAMM_unplanned_30days.txt")
+sink(paste0("../../outputs/summary_GAMM_unplanned_30days",params$state,".txt"))
 print(summary(model_unplanned_30days_bl))
 print(paste0('For component hospital_st, variance is: ',round(var_re_unplanned_30d$variance,5), 
              ' with lower_ci95=',round(var_re_unplanned_30d$lower_ci,5),' and upper_ci95=',round(var_re_unplanned_30d$upper_ci,5)))
